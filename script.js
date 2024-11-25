c3826ea120b24b483904366f7d8e6b8adfdef7ba
Ming-99s <lyming4999@gmail.com>
2024-11-25 12:58:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function scroll() {
+    let navbar = document.querySelector('.navbar');
+    
+    if (window.scrollY > 1) {
+        navbar.classList.add('scroll');
+    } else {
+        navbar.classList.remove('scroll');
+    }
+};
+let observer = new IntersectionObserver(function(entries){
+    entries.forEach(function(entry){
+        if(entry.isIntersecting){
+            entry.target.classList.add('visible');
+            }
+        });
+    });
+
+window.onscroll = scroll;
+
+let hiddenE= document.querySelectorAll('.text-side');
+let hiddenImg = document.querySelectorAll('.img');
+let hiddenImg2 = document.querySelectorAll('#box-img');
+
+hiddenE.forEach(function(element1){observer.observe(element1)});
+hiddenImg.forEach(function(element2){observer.observe(element2)});
+hiddenImg2.forEach(function(element3){observer.observe(element3)});
